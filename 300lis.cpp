ccf2e59097d2fd3e0ab5ca8bfbd4e5f1562d8812
Shivam Balikondwar <shivambalikondwar101@gmail.com>
2021-10-25 23:52:51 +0530

    Create 300lis.cpp

---
+++ 300lis.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int mx(int a, int b){
+        return a>=b?a:b;
+    }
+    
+    vector<vector<int>> dp;
+    int lengthOfLIS(vector<int>& nums) {
+        dp.resize(size(nums), vector<int>(1+size(nums), -1));   
+        
+        return solve(nums, 0, -1);
+    }
+    
+    int solve(vector<int>& nums, int i, int prev_i) {
+        if(i >= size(nums)) return 0;
+        if(dp[i][prev_i+1] != -1) return dp[i][prev_i+1];
+        
+        int take = 0, dontTake = solve(nums, i + 1, prev_i);
+        if(prev_i == -1 || nums[i] > nums[prev_i]) take = 1 + solve(nums, i + 1, i); 
+        
+        return dp[i][prev_i+1] = max(take, dontTake);
+    }
+};
